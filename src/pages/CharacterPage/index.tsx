e3406eeac58f7cdb16b336570780a78691209b3c
Tommy Cosby <cosbyts@gmail.com>
2019-07-22 00:16:31 -0700

    ACTUALLY FIX the damned env variable

--- src/pages/CharacterPage/index.tsx
+++ src/pages/CharacterPage/index.tsx
@@ -60,7 +60,7 @@ const searchForCharacter = (characterName: string, realmSlug: string) => {
   let apiLink =
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:4000/blizzard/character'
-      : `${process.env.RAIDTEAM_API_URL}/blizzard/character`;
+      : `${process.env.REACT_APP_RAIDTEAM_API_URL}/blizzard/character`;
   return axios.get(apiLink || '', { params });
 };
 
