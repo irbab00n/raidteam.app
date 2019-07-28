93a4092f4cef6b98ad839705e561eee226a7fe42
Thomas Cosby <cosbyts@gmail.com>
2019-07-27 20:49:16 -0700

    Add basic CharacterPage loading logic

--- src/pages/CharacterPage/index.tsx
+++ src/pages/CharacterPage/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import queryString from 'query-string';
 import './_CharacterPage.scss';
 
 import { CharacterHeader } from '../../components/CharacterPage';
@@ -46,24 +47,65 @@ const searchForCharacter = (characterName: string, realmSlug: string) => {
 };
 
 const CharacterPage = (props: CharacterPageProps) => {
-  const [characterName, setCharacterName] = useState('');
-  const [realmName, setRealmName] = useState('');
+  const parsedSearch = queryString.parse(window.location.search);
+
+  const _initialCharacterName = parsedSearch.character || '';
+  const _initialRealmName = parsedSearch.realm || '';
+
+  const [searchFailed, setSearchFailed] = useState<any>(false);
+  const [searchFailedMessage, setSearchFailedMessage] = useState<any>('');
+  const [searchingForCharacter, setSearchingForCharacter] = useState<any>(false);
+  const [characterName, setCharacterName] = useState<any>(_initialCharacterName);
+  const [realmName, setRealmName] = useState<any>(_initialRealmName);
   const { match, characterData, setCharacterData } = props;
 
+  console.log('parsedSearch from query string: ', parsedSearch);
   console.log('match object on character page: ', match);
+
+  const characterSearch = () => {
+    console.log('running character search');
+    setSearchFailed(false);
+    setSearchingForCharacter(true);
+    let realmSlug = findRealmSlug(realmName);
+    if (realmSlug !== null) {
+      searchForCharacter(characterName, realmSlug)
+        .then(result => {
+          setSearchingForCharacter(false);
+          setCharacterData(result.data);
+          console.log('result from search for character: ', result.data);
+        })
+        .catch(error => {
+          setSearchingForCharacter(false);
+          setSearchFailed(true);
+          console.error(error);
+          setSearchFailedMessage(
+            `Something went wrong fetching character data.  Please try again...`
+          );
+        });
+    } else {
+      setSearchingForCharacter(false);
+      setSearchFailed(true);
+      setSearchFailedMessage(
+        `You didn't enter in a valid Realm name.  Yes, it has to be Capitol specific... for now.`
+      );
+    }
+  };
 
   const handleCharacterSearch = e => {
     e.preventDefault();
-    let realmSlug = findRealmSlug(realmName);
-    if (realmSlug !== null) {
-      searchForCharacter(characterName, realmSlug).then(result => {
-        setCharacterData(result.data);
-        console.log('result from search for character: ', result.data);
-      });
-    } else {
-      // console.log(`Couldn't find the realm for the target you searched for: `, realmName);
-    }
+    characterSearch();
   };
+
+  useEffect(() => {
+    console.log('UseEffect running on characterPage');
+    if (characterName !== '' && characterData === null) {
+      characterSearch();
+    }
+
+    return () => {
+      console.log('Cleaning up CharacterPage');
+    };
+  }, [characterData]);
 
   return (
     <div id="character-page">
@@ -108,6 +150,21 @@ const CharacterPage = (props: CharacterPageProps) => {
         }
       />
       <GridWrapper>
+        {characterData === null && !searchingForCharacter && (
+          <div className="cp_no-character-prompt">
+            <h1>Find a Specific Raider</h1>
+          </div>
+        )}
+        {searchingForCharacter && (
+          <div className="cp_no-character-prompt">
+            <h1>Loading...</h1>
+          </div>
+        )}
+        {searchFailed && (
+          <div className="cp_no-character-prompt">
+            <div>{searchFailedMessage}</div>
+          </div>
+        )}
         {characterData !== null && (
           <GridItem size="half">
             <CharacterDisplay characterData={characterData} />
